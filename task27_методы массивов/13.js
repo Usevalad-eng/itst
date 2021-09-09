4a06449bb3db24dc455dd00f6aeca01724dcebca
Usevalad-eng <avm012@gmail.com>
2021-09-09 17:39:00 +0300

    0909211738

---
+++ task27_методы массивов/13.js
@@ -0,0 +1,7 @@
+let arr = [3, 4, 1, 2, 7];
+
+
+
+arr.sort();
+
+console.log(arr);
---
+++ task27_методы массивов/15.js
@@ -0,0 +1,7 @@
+let array1 = [3, 3, 1, 2, 7];
+sum = 0;
+array1.forEach(func);
+function func(value){
+  sum += value;
+}
+console.log(sum);

a36e9d41bdb4744aa5daaa41b171cc83cb2fd607
Usevalad-eng <avm012@gmail.com>
2021-11-04 21:21:22 +0300

    er

--- task42_ДОМ_ивентс/js/script4.js
+++ task42_ДОМ_ивентс/js/script4.js
@@ -1,7 +1,7 @@
 document.getElementById("calc").onclick = function(){
-    let x = parseFloat(document.getElementById("a").value);
-    let y = parseFloat(document.getElementById("b").value); 
-    //let z = parseFloat(document.getElementById("d").value);
+    let x = document.getElementById("a").value;
+    let y = document.getElementById("b").value; 
+    
     x = z.value;
     y = x.value;
     x = z.value;   

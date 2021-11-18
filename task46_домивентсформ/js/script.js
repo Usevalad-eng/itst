11c17fecb3336b781f88d46a66c0bdbc12983121
Usevalad-eng <avm012@gmail.com>
2021-11-18 21:22:48 +0300

    181121

---
+++ task46_домивентсформ/js/script.js
@@ -0,0 +1,16 @@
+document.body.onkeydown = function(event){
+    if (event.ctrlKey) {
+        let classes = document.getElementsByTagName("div").item(0).classList;
+        let ol = document.createElement("ol");
+        for(let i = 0;i < 1; i++){
+        let li = document.createElement("li");
+        li.textContent = "пункт";
+        ol.append(li);
+        document.getElementsByTagName("div").item(0).append(ol);
+    }
+    }
+    
+    
+}
+
+

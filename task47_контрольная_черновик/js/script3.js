f7f05fd9c42cb70cb25cac51a71c9fe81b83f7ee
Usevalad-eng <avm012@gmail.com>
2021-12-02 07:38:17 +0300

    021221

--- task47_контрольная_черновик/js/script3.js
+++
@@ -1,9 +0,0 @@
-colors.onchange = function(event){
-    let color = this.value;
-    document.querySelector("div").style.background = color;
-}
-
-let options = document.querySelectorAll("option");
-options.forEach(element => {
-    element.style.background = element.value;
-});

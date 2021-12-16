056bbe556db2842f1dc8cd093ddddce2a5a0ded5
Usevalad-eng <avm012@gmail.com>
2021-12-16 21:28:04 +0300

    dsfasd

---
+++ task52_лаб/js/script1_2.js
@@ -0,0 +1,63 @@
+
+/*
+
+document.getElementById("calc").onclick = function(){
+    let classes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    x = document.getElementById("a").value;
+    let ol = document.createElement("ul");
+    for(let i = 0;i < x; i++){
+        let li = document.createElement("li");
+        li.textContent = classes[i];
+        ol.append(li);
+        document.getElementsByTagName("div").item(0).append(ol);
+    }
+    
+}
+
+document.getElementById("calc2").onclick = function(){
+    
+    let y = document.getElementById("b").value;
+   
+    //y = y.split("-").map(item => item[0] + item[2]).join(".");
+    arr = y.split(" ").map(function(item){
+        return +item;
+    }).reduce((sum, current) => sum + current);
+
+    //function arraySum(array){
+        //var sum = 0;
+       // for(var i = 0; i < array.length; i++){
+            //sum += array[i];
+            //}
+        
+       //}
+        //arraySum(arr);
+        
+        document.getElementsByTagName("div").item(1).innerHTML = arr;
+    
+    
+}
+*/
+
+document.getElementById("calc3").onclick = function(){
+    
+    let z = document.getElementById("bb").value;
+    let zz = document.getElementById("bbb").value;
+   
+    
+    let arr = z.split("").sort();
+      let arr2 = zz.split("").sort();
+      for(let i = 0;i < z.length; i++){
+        if(arr[i] ==arr2[i]){
+        } else{
+            return false;
+        }
+
+    }
+    return true;        
+        //document.getElementsByTagName("div").item(1).innerHTML = r;
+    
+    
+}
+
+
+

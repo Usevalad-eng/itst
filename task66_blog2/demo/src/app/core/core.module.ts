802ae1115c3b8261fd0bbd12d061ff524d89f4cc
гк <@adamant.local>
2022-03-24 21:22:42 +0300

    240322

---
+++ task66_blog2/demo/src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostService, CommentService, UserService  } from './service';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+providers: 
+})
+export class CoreModule { }

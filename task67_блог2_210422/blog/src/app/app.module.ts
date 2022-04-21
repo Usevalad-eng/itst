8e5c6be1afe9c3d8afc4e806897f2150237c3b83
гк <@adamant.local>
2022-04-21 20:32:24 +0300

    210422

---
+++ task67_блог2_210422/blog/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
+import { FooterComponent, HeaderComponent, SharedModule} from './shared';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    HomeModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ task67_блог2_210422/blog/src/app/user/userdetail/userdetail.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, Route, Router } from '@angular/router';
+import { User, UserService } from 'src/app/core';
+
+@Component({
+  selector: 'app-userdetail',
+  templateUrl: './userdetail.component.html',
+  styleUrls: ['./userdetail.component.scss']
+})
+export class UserDetailComponent implements OnInit {
+
+    user: User = {} as User;
+   
+    constructor(private route: ActivatedRouteSnapshot, private userService: UserService) { 
+
+    }
+
+  ngOnInit(): void {
+    this.userService.getById(this.route.params["id"]).subscribe(
+      (data) => {
+        this.user = data;
+      }
+    );
+  }
+
+}
